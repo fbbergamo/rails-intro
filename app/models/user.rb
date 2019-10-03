7f75fb024116fb44ea61000d49f10b5270cd45a4
Felipe Bergamo <fbbergamo@gmail.com>
2019-10-03 15:22:55 +0000

    rails test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  extend  ActionView::Helpers::DateHelper
+  
   has_many :microposts, dependent: :destroy
   validates :email, presence: true    
   validates :name, presence: true
+  
+  def days_since_created
+    Date.today.mjd - created_at.to_date.mjd
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     travel_to DateTime.new(2019, 01, 10) do
+        user = users(:one)
+        assert_equal(user.days_since_created, 9)
+     end
+   end
 end
